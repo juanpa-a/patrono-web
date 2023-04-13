ccb903adf26b1cd263d31aafb75b8176f2de951e
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2023-04-12 21:58:37 -0600

    renamed home to index

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e0af9249824894f"; }
+/******/ 		__webpack_require__.h = function() { return "bf360760c52581e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
